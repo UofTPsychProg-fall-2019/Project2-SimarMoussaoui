16403ed3a4ade7d806aef0cf4e48e436d8e1ca66
SimarMoussaoui <55145484+SimarMoussaoui@users.noreply.github.com>
2020-04-13 15:36:02 -0400

    Update MassAddSaccade.m

--- MassAddSaccade.m
+++ MassAddSaccade.m
@@ -1,9 +1,12 @@
 function MassAddSaccade()
 
-ptcpList = [52,53,54,57,58,59,60,65,68,71];
+ptcpList = [2]
+% ptcpList = [52,53,54,57,58,59,60,65,68,71];
 % ptcpList = [2,4,5,7,8,9,11,14,15,23,26,30,32,34,35,36,37,38,39,41,42,43,44,48,50,52,53,54,57,58,59,60,65,68,71];
 blockList = [1, 2, 3];
 
+% This function will allow me to add the vertical, horizontal, and directions of saccades for each block for every participant
+% in one go.
 for iptcp = 1:length(ptcpList)
     subNum = ptcpList(iptcp);
 
